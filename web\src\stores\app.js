0b6cba893090ea161aed13f352a6e0614d3a3a2c
guaihaizi11 <162794378+guaihaizi11@users.noreply.github.com>
2026-07-06 22:44:23 +0800

    Add web\src\stores\app.js

---
+++ web\src\stores\app.js
@@ -0,0 +1,33 @@
+﻿import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
+import api from '@/utils/api'
+
+export const useAppStore = defineStore('app', () => {
+  const sidebarCollapsed = ref(false)
+  const notifications = ref([])
+  const onlineChannels = ref(0)
+  const totalChannels = ref(0)
+  const unreadAlerts = ref(0)
+
+  const toggleSidebar = () => { sidebarCollapsed.value = !sidebarCollapsed.value }
+
+  const addNotification = (msg) => {
+    notifications.value.unshift({
+      id: Date.now(),
+      message: msg,
+      time: new Date().toLocaleString(),
+      read: false
+    })
+    if (notifications.value.length > 50) notifications.value.pop()
+  }
+
+  const markRead = (id) => {
+    const n = notifications.value.find(n => n.id === id)
+    if (n) n.read = true
+  }
+
+  return {
+    sidebarCollapsed, notifications, onlineChannels, totalChannels, unreadAlerts,
+    toggleSidebar, addNotification, markRead
+  }
+})
