eb90833af34126cd21c934c1970c02273dca8460
guaihaizi11 <162794378+guaihaizi11@users.noreply.github.com>
2026-07-06 22:44:27 +0800

    Add web\src\utils\api.js

---
+++ web\src\utils\api.js
@@ -0,0 +1,24 @@
+﻿import axios from 'axios'
+
+const api = axios.create({
+  baseURL: '/api',
+  timeout: 15000,
+  headers: { 'Content-Type': 'application/json' }
+})
+
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('ev_token')
+  if (token) config.headers.Authorization = 'Bearer ' + token
+  return config
+})
+
+api.interceptors.response.use(
+  response => response.data,
+  error => {
+    const msg = error.response?.data?.detail || error.message || 'Request failed'
+    console.error('API Error:', msg)
+    return Promise.reject(error)
+  }
+)
+
+export default api
